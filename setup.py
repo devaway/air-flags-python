86cbe9738237f66a3d0d21126422b9d90d864561
Ruben Espinosa <ruben@devaway.io>
2021-12-16 08:50:37 +0100

    Automate releases and set version (#25)
    * Update setup and add release workflow
    
    * Update GA

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
-VERSION = "0.1.0"
+VERSION = "0.1.4"
 DESCRIPTION = "Air flags python library"
 LONG_DESCRIPTION = "Air flags library to manage Python feature flags"
 
@@ -16,21 +15,23 @@ setup(
     author_email="airflags@devaway.es",
     license="BSD",
     install_requires=[
-        str(requirement)
-        for requirement in parse_requirements("requirements.txt")
+        "pyyaml==6.0",
+        "jsonschema==4.2.1"
     ],
-    tests_require=[
-        str(test_requirement)
-        for test_requirement in parse_requirements("requirements-test.txt")
-    ],
-    python_requires=">=3.6",
+    python_requires=">=3.4",
     url="https://github.com/devaway/air-flags-python",
     project_urls={
         "Bug Tracker": "https://github.com/devaway/air-flags-python/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
